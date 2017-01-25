f3eadeb47646373b5c00a6145f055f926c051771
Vayne-Lover <406378362@qq.com>
2017-01-25 22:52:01 +0800

    Add data and displayWork function.

--- Online Resume/js/resumeBuilder.js
+++ Online Resume/js/resumeBuilder.js
@@ -4,22 +4,93 @@ This is empty on purpose! Your code to build the resume will go here.
 var bio={
 	"name":"Vayne",
 	"role":"Deep Learning Engineer",
-	"picture'":"../images/fry.jpg"
+	"picture'":"../images/fry.jpg",
+	"welcomeMessage":"Hello",
+	"skills":[1,2,3,4,5],
+	"contacts":{
+		"mobile":111,
+		"email":"aaa@qq.com",
+		"github":"github",
+		"twitter":"twitter",
+		"location":"San Franciso"
+	}
 };
 
-var work={};
-work.position="Developer";
-work.employer="Udacity";
-work.years=3;
+var education={
+	"schools":[
+		{
+			"name":"Nova Southeastern University",
+			"location":"Fort Lauderdale, FL",
+			"degree":"master",
+			"major":["CS"],
+			"dates":2013,
+			"url":"url"
+		},
+		{
+			"name":"Nova Southeastern University",
+			"location":"Fort Lauderdale, FL",
+			"degree":"master",
+			"major":["CS"],
+			"dates":2013,
+			"url":"url"	
+		}	
+		],
+	"onlineCourses":[
+		{
+			"title":"JS",
+			"school":"Udacity",
+			"dates":2014,
+			"url":"url"
+		}
+	]	
+};
 
-var education={};
-education.name="University";
-education.years="2015-2017";
-education.city="Zhejiang,Hangzhou";
+var work={
+	"jobs":[
+		{
+			"employer":"Planet",
+			"title":"Delivery",
+			"dates":"Jan 30-Future",
+			"description":"As a young privileged girl in the heart of Demacia's elite, her father tried to convince her of the constabulary's ever-vigilant eye. Young and naive, she truly believed that her world was one of perfect safety, until one night, when a twisted witch took interest in her father. The malevolent woman overcame her father's conciliar guard, then tortured her family before murdering them. The young Shauna escaped only by hiding herself and then fleeing once the hag had departed, plagued by the screams of her loved ones as she ran. A burning hatred was born in her that day, one that could never be denied."
+		},
+		{
+			"employer":"Planet2",
+			"title":"Delivery2",
+			"dates":"Jul 30-Jan 30",
+			"description":"As a young privileged girl in the heart of Demacia's elite, her father tried to convince her of the constabulary's ever-vigilant eye. Young and naive, she truly believed that her world was one of perfect safety, until one night, when a twisted witch took interest in her father. The malevolent woman overcame her father's conciliar guard, then tortured her family before murdering them. The young Shauna escaped only by hiding herself and then fleeing once the hag had departed, plagued by the screams of her loved ones as she ran. A burning hatred was born in her that day, one that could never be denied."
+		},
+	]
+};
 
-$("#main").append(work.position);
-$("#main").append(education.name);
+var projects={
+	"projects":[
+		{
+			"title":"sample proj 1",
+			"dates":"2014",
+			"description":"As a young privileged girl in the heart of Demacia's elite, her father tried to convince her of the constabulary's ever-vigilant eye. Young and naive, she truly believed that her world was one of perfect safety, until one night, when a twisted witch took interest in her father. The malevolent woman overcame her father's conciliar guard, then tortured her family before murdering them. The young Shauna escaped only by hiding herself and then fleeing once the hag had departed, plagued by the screams of her loved ones as she ran. A burning hatred was born in her that day, one that could never be denied.",
+			"images":[
+				"https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+				"https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg"
+			]
+		}
+	]
+};
 
-$("#header").append(HTMLheaderName.replace("%data%","Vayne Lover"));
-$("#header").prepend(HTMLheaderRole.replace("%data%","Deep Learning"));
-					
+
+function displayWork(){
+for (job in work.jobs){
+	$("#workExperience").append(HTMLworkStart);
+	var formattedEmployer=HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+	var formattedTitle=HTMLworkTitle.replace("%data%",work.jobs[job].title);
+	var formattedEmployerTitle=formattedEmployer+formattedTitle;
+	$(".work-entry:last").append(formattedEmployerTitle);
+	
+	var formattedDates=HTMLworkDates.replace("%data%",work.jobs[job].dates);
+	$(".work-entry:last").append(formattedDates);
+	
+	var formattedDescription=HTMLworkDates.replace("%data%",work.jobs[job].description);
+	$(".work-entry:last").append(formattedDescription);
+}
+}
+
+displayWork();
